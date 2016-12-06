57e671bd5ce9a819efcb8e8d166230119aa43967
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-12-06 13:49:04 +0100

    New upstream version 0.4.5

--- setup.py
+++ setup.py
@@ -28,15 +28,14 @@ from setuptools import setup
 
 setup(
     name='msrestazure',
-    version='0.4.4',
+    version='0.4.5',
     author='Microsoft Corporation',
     packages=['msrestazure'],
-    url=('https://github.com/Azure/autorest/tree/master/'
-         'src/client/Python/msrestazure'),
+    url='https://github.com/Azure/msrestazure-for-python',
     license='MIT License',
     description=('AutoRest swagger generator Python client runtime. '
                  'Azure-specific module.'),
-    long_description=open('readme.rst').read(),
+    long_description=open('README.rst').read(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
@@ -49,5 +48,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development'],
     install_requires=[
-        "msrest>=0.4.4"],
+        "msrest~=0.4.4",
+        "keyring>=5.6",
+        "adal~=0.4.0"
+    ],
 )
